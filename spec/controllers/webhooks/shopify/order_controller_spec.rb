cee78db80b7bb33e0161e7328379ca5c97b85bdb
Kaarle Kulvik <k.kulvik@gmail.com>
2025-01-04 18:57:19 +0100

    Order creation finished with tests (#44)
    Co-authored-by: Kaarle Kulvik <kaarle@supersaas.com>

--- spec/controllers/webhooks/shopify/order_controller_spec.rb
+++ spec/controllers/webhooks/shopify/order_controller_spec.rb
@@ -21,48 +21,6 @@ RSpec.describe(Webhooks::Shopify::OrderController, type: :controller) do
   end
 
   describe 'POST #fulfill' do
-  #   context 'with valid data' do
-  #     it 'creates a new user' do
-  #       expect {
-  #         post(:create, body: user_created)
-  #       }.to(change(User, :count).by(1))
-  #
-  #       user_json = JSON.parse(user_created, symbolize_names: true)
-  #       expect(response).to(have_http_status(:ok))
-  #       user_last = User.last
-  #       expect(user_last.email).to(eq(user_json[:email]))
-  #       expect(user_last.address_1).to(eq(user_json[:default_address][:address1]))
-  #       expect(user_last.phone).to(eq(user_json[:default_address][:phone]))
-  #       expect(user_last.shopify_id).to(eq(user_json[:id].to_s))
-  #       expect(user_last.phone_prefix).to(eq(27))
-  #     end
-  #
-  #     it 'enqueues a slack notification for user' do
-  #       expect {
-  #         post(:create, body: user_created)
-  #       }.to(change(SlackNotificationJob.jobs, :size).by(1))
-  #     end
-  #
-  #     it 'updates existing user shopify_id' do
-  #       user_json = JSON.parse(user_created, symbolize_names: true)
-  #       user = create(:user, email: user_json[:email])
-  #
-  #       expect {
-  #         post(:create, body: user_created)
-  #       }.not_to(change(User, :count))
-  #
-  #       expect(user.reload.shopify_id).to(eq(user_json[:id].to_s))
-  #       expect(response).to(have_http_status(:ok))
-  #     end
-  #   end
-  #
-  #   context 'with invalid data' do
-  #     it 'returns bad request for invalid JSON' do
-  #       post :create, body: 'invalid json'
-  #       expect(response).to(have_http_status(:bad_request))
-  #     end
-  #   end
-
     context 'with invalid JSON' do
       it 'returns bad request' do
         post :fulfill, body: 'invalid json'
@@ -90,108 +48,79 @@ RSpec.describe(Webhooks::Shopify::OrderController, type: :controller) do
     context 'without customer data' do
       let(:order_fulfilled_no_customer_raw_post_data) { file_fixture('shopify/order_fulfilled.json').read }
 
-      it 'creates order with email only user' do
+      it 'creates order and user with only email' do
           expect { post(:fulfill, body: order_fulfilled_no_customer_raw_post_data) }
-            .to(change(Order, :count).by(1))
-            .and(change(User, :count).by(1))
-            .and(change(SlackNotificationJob.jobs, :size).by(1))
+            .to(change(Order, :count).by(1)
+            .and(change(User, :count).by(1)
+            .and(change(SlackNotificationJob.jobs, :size).by(2))))
+
+          expect(response).to(have_http_status(:ok))
 
           user = User.last
           expect(user.email).to(eq('hello@fresh.com'))
-          expect(user.shopify_id).to(be_nil)
+          expect(user.shopify_id).to(eq(""))
 
-          # expect {
-          #   post(:create, body: order_fulfilled_no_customer_raw_post_data)
-          # }
+          order = Order.last
+          expect(order.product_type).to(eq("02"))
+          expect(order.quantity).to(eq(5))
 
-          last_job = SlackNotificationJob.jobs.last
-          expect(last_job["args"]).to(eq([ "User created without ShopifyID hello@fresh.com" ]))
+          last_jobs = SlackNotificationJob.jobs.last(2)
+          expect(last_jobs[0]["args"]).to(eq([ "User created without ShopifyID hello@fresh.com" ]))
+          expect(last_jobs[1]["args"]).to(eq([ "Order fulfilled: hello@fresh.com, id 6683595997448, sku 02" ]))
+      end
+
+      context 'with an existing customer' do
+        it 'creates order with customer' do
+          create(:user, email: 'hello@fresh.com')
+
+          expect {
+            post(:fulfill, body: order_fulfilled_no_customer_raw_post_data)
+          }.to(change(Order, :count).by(1)
+           .and(not_change(User, :count)))
+
+          expect(response).to(have_http_status(:ok))
+        end
       end
     end
 
-          #   it 'uses existing user if found by email' do
-          #     existing_user = create(:user, email: 'hello@fresh.com')
-          #
-          #     expect {
-          #       post(:fulfill, body: webhook_without_customer, headers: valid_headers)
-          #     }.to(change(Order, :count).by(1)
-          #                               .and(not_change(User, :count)))
-          #
-          #     expect(existing_user.reload.shopify_id).to(be_nil)
-          #   end
-          # end
+    context 'with customer data in payload' do
+      let(:order_fulfilled_customer_raw_post_data) { file_fixture('shopify/order_fulfilled_customer.json').read }
 
+      context 'without existing customer' do
+        it 'creates order and customer' do
+          expect { post(:fulfill, body: order_fulfilled_customer_raw_post_data) }
+            .to(change(Order, :count).by(1)
+            .and(change(User, :count).by(1)
+            .and(change(SlackNotificationJob.jobs, :size).by(2))))
 
+          expect(response).to(have_http_status(:ok))
 
+          user = User.last
+          expect(user.email).to(eq('john@example.com'))
+          expect(user.shopify_id).to(eq("115310627314723954"))
 
-#
-#     context 'with fulfilled order' do
-#       let(:webhook_with_customer) { file_fixture('fulfilled_order_with_customer.json').read }
-#       let(:webhook_without_customer) { file_fixture('fulfilled_order_without_customer.json').read }
-#
-#       context 'with customer data' do
-#         it 'creates order and user with full details' do
-#           expect {
-#             post(:fulfill, body: webhook_with_customer, headers: valid_headers)
-#           }.to(change(Order, :count).by(1)
-#                                     .and(change(User, :count).by(1)))
-#
-#           user = User.last
-#           expect(user.email).to(eq('john@example.com'))
-#           expect(user.shopify_id).to(eq('115310627314723954'))
-#           expect(user.address_1).to(eq('123 Elm St.'))
-#
-#           order = Order.last
-#           expect(order.product_type).to(eq('02'))
-#           expect(order.quantity).to(eq(5))
-#           expect(order.order_status).to(eq('fulfilled'))
-#           expect(order.user_id).to(eq(user.id))
-#
-#           expect(slack_job).to(have_received(:perform_async)
-#                                  .with('User created in Shopify: john@example.com'))
-#         end
-#
-#         it 'updates existing user if found' do
-#           existing_user = create(:user, email: 'john@example.com')
-#
-#           expect {
-#             post(:fulfill, body: webhook_with_customer, headers: valid_headers)
-#           }.to(change(Order, :count).by(1)
-#                                     .and(not_change(User, :count)))
-#
-#           expect(existing_user.reload.shopify_id).to(eq('115310627314723954'))
-#         end
-#       end
-#
-#
-#
-#       context 'without customer and email' do
-#         it 'creates order without user' do
-#           webhook_data = JSON.parse(webhook_without_customer)
-#           webhook_data['email'] = nil
-#
-#           expect {
-#             post(:fulfill, body: webhook_data.to_json, headers: valid_headers)
-#           }.to(change(Order, :count).by(1)
-#                                     .and(not_change(User, :count)))
-#
-#           expect(Order.last.user_id).to(be_nil)
-#           expect(slack_job).to(have_received(:perform_async)
-#                                  .with("Order fulfilled without a customer and email #{webhook_data['id']}"))
-#         end
-#       end
-#
-#       context 'with invalid order data' do
-#         before do
-#           allow_any_instance_of(Order).to(receive(:save).and_return(false))
-#         end
-#
-#         it 'returns unprocessable_entity' do
-#           post :fulfill, body: webhook_with_customer, headers: valid_headers
-#           expect(response).to(have_http_status(:unprocessable_entity))
-#         end
-#       end
-#     end
-#   end
+          order = Order.last
+          expect(order.product_type).to(eq("02"))
+          expect(order.quantity).to(eq(5))
+
+          last_jobs = SlackNotificationJob.jobs.last(2)
+          expect(last_jobs[0]["args"]).to(eq([ "User created in Shopify: john@example.com, 115310627314723954" ]))
+          expect(last_jobs[1]["args"]).to(eq([ "Order fulfilled: john@example.com, id 6683595997448, sku 02" ]))
+        end
+      end
+    end
+
+    context 'with an existing customer' do
+      it 'creates order' do
+        create(:user, email: 'john@example.com')
+
+        expect {
+          post(:fulfill, body: order_fulfilled_customer_raw_post_data)
+        }.to(change(Order, :count).by(1)
+         .and(not_change(User, :count)))
+
+        expect(response).to(have_http_status(:ok))
+      end
+    end
   end
 end
